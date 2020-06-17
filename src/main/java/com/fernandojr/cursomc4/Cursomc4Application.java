5362d6900a00ade2da4300dce1998587135d6488
fpjunior <fpjuniorpx@gmail.com>
2020-06-17 10:07:02 -0300

    Projeto criado

---
+++ src/main/java/com/fernandojr/cursomc4/Cursomc4Application.java
@@ -0,0 +1,13 @@
+package com.fernandojr.cursomc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandojr/cursomc4/Cursomc4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandojr.cursomc4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
