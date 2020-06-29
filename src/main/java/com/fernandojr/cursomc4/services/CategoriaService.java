7cfe215c904d5307c27325328c88ceb11116b456
fpjunior <fpjuniorpx@gmail.com>
2020-06-29 18:16:00 -0300

    crie classe de serviços

--- src/main/java/com/fernandojr/cursomc4/services/CategoriaService.java
+++ src/main/java/com/fernandojr/cursomc4/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandojr.cursomc4.domain.Categoria;
+import com.fernandojr.cursomc4.exceptions.ObjectNotFoundException;
 import com.fernandojr.cursomc4.repositories.CategoriaRepository;
 
 @Service
@@ -13,14 +14,17 @@ public class CategoriaService {
 	
 //	Anotação Autowired faz com que as dependencias sejam automaticamente estanciada pelo spring, pelo mecanismo de injeção de 
 //	depencia ou inversão de controles
+//	O metodo abaixo precisa receber um id como parametro
+//	ele fara uma busca por id
+	
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-//	O metodo abaixo precisa receber um id como parametro
-//	ele fara uma busca por id
 	public Categoria buscarCategoriaPorId(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
