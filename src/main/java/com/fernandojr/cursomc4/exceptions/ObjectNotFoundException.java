de4f9b7d87570fbaf51672467e7831c260e6b4aa
fpjunior <fpjuniorpx@gmail.com>
2020-06-29 18:20:34 -0300

    crie classe ObjectNotFoundExcpetion para tratamento de erros

---
+++ src/main/java/com/fernandojr/cursomc4/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.fernandojr.cursomc4.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
